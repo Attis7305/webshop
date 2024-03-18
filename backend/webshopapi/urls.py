1b132ab112d955f3cab4cf31b9e1c93aa59a7cb6
Joe <nalt@freemail.hu>
2024-03-18 06:58:56 +0100

    My forked commit 2

--- backend/webshopapi/urls.py
+++ backend/webshopapi/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from .import views
 
 urlpatterns = [
-    #TODO
+    path('',views.store, name="store"),
+    path('cart/',views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
